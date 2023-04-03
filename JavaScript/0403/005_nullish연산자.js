61e545035173962b35b712cc2d4d989a2547d8f6
ShinEun9 <ses2201@naver.com>
2023-04-03 16:56:30 +0900

    0403 number, boolean, undefined, null 공부

---
+++ JavaScript/0403/005_nullish연산자.js
@@ -0,0 +1,32 @@
+let firstName = null;
+let lastName = null;
+let nickName = "licat";
+
+console.log(firstName ?? nickName);
+console.log(firstName ?? lastName ?? nickName);
+console.log(firstName ?? lastName ?? nickName ?? "익명사용자");
+
+let a = null;
+let b = 10;
+let c = null;
+
+console.log(a ?? b ?? c);
+
+// 단락회로평가와 nullish 연산자 차이점
+let height = 0;
+console.log(height || 100); // 100
+console.log(height ?? 100); // 0
+
+let height2;
+console.log(height2 || 100); // 100
+console.log(height2 ?? 100); // 100
+
+let height3 = "";
+console.log(height3 || "hello"); // hello
+console.log(height3 ?? "world"); // ""
+
+// || : 0, null, undefined
+// ?? : null, undefined
+
+// falsy하다: 0, null, undefined, "", NaN
+// nullish하다: null, undefined
---
+++ JavaScript/0403/006_json-data.js
@@ -0,0 +1,60 @@
+let 회원정보 = [
+  {
+    _id: "642a624f6ec3b5279060041a",
+    index: 0,
+    age: 20,
+    eyeColor: "brown",
+    name: "Stark Larsen",
+    gender: "male",
+    company: "CINESANCT",
+  },
+  {
+    _id: "642a624f6bbb7dfd17cb6648",
+    index: 1,
+    age: 37,
+    eyeColor: "blue",
+    name: "Madeline Wright",
+    gender: "female",
+    company: "ANIMALIA",
+  },
+  {
+    _id: "642a624f1c330b5f808c0be6",
+    index: 2,
+    age: 30,
+    eyeColor: "blue",
+    name: "Newton Leblanc",
+    gender: "male",
+    company: "UNCORP",
+  },
+  {
+    _id: "642a624fe039971b3ee19882",
+    index: 3,
+    age: 39,
+    eyeColor: "green",
+    name: "Wells Lindsey",
+    gender: "male",
+    company: "INTERLOO",
+  },
+  {
+    _id: "642a624f36268c3c3e8388f6",
+    index: 4,
+    age: 40,
+    eyeColor: "green",
+    name: "Teri White",
+    gender: "female",
+    company: "COMVOY",
+  },
+  {
+    _id: "642a624ff34b9537ac3ddc1a",
+    index: 5,
+    age: 21,
+    eyeColor: "green",
+    name: "Alba Hampton",
+    gender: "female",
+    company: "AQUOAVO",
+  },
+];
+
+console.log(회원정보[0]);
+console.log(회원정보[0]["name"]);
+console.log(회원정보[0]["company"]);
