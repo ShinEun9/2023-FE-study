2aec149927323beb2d336bd316c8263a7734522a
ShinEun9 <ses2201@naver.com>
2023-04-22 22:35:24 +0900

    0422 tdz 공부

---
+++ Practice/tdz.js
@@ -0,0 +1,13 @@
+a = 3; // 할당
+console.log(a);
+let a = 1;
+console.log(a);
+
+// 선언과 할당 다 같이 된 게 초기화
+// let a = 3;
+
+// let a;
+// a = 3;
+// console.log(a); // 3
+// a = 1;
+// console.log(a); // 1
