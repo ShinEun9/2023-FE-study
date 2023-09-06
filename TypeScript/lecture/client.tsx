cd1909821eee80d1ee39f885c81183f9bb349c77
ShinEun9 <ses2201@naver.com>
2023-09-06 18:30:02 +0900

    0906 이벤트핸들러, useRef, useCallback, Class 타이핑

---
+++ TypeScript/lecture/client.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import GuGuDan from "./components/GuGuDan";
+import WordRelay from "./components/WordRelay";
+
+ReactDOM.render(<WordRelay />, document.querySelector("#root"));
